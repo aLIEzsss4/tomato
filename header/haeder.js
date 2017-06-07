4835654c41128b8f7ce6df154407489c0a8efb80
aLIEzsss4 <15624958088@163.com>
2017-06-07 13:10:51 +0800

    Add files via upload

--- header/haeder.js
+++ header/haeder.js
@@ -7,22 +7,22 @@ window.onload=function(){
         if (e.wheelDelta) {  //判断浏览器IE，谷歌滑轮事件                 
             if (e.wheelDelta > 0) { //当滑轮向上滚动时     
 //              alert("滑轮向上滚动");  
-                search.style.top=0;
-                nav.style.top=-80+'px';
+                search.style.top=-80+'px';
+                nav.style.top=0;
             }  
             if (e.wheelDelta < 0) { //当滑轮向下滚动时   
-                search.style.top=80+'px'; 
-                  nav.style.top=0+'px';
+                search.style.top=0+'px';
+                  nav.style.top=80+'px'; 
             }  
         } else if (e.detail) {  //Firefox滑轮事件  
             if (e.detail> 0) { //当滑轮向上滚动时  
-                    nav.style.top=-80+'px';
-                search.style.top=0;
+                    nav.style.top=0;
+                search.style.top=-80+'px';
                 
             }  
             if (e.detail< 0) { //当滑轮向下滚动时  
-               search.style.top=80+'px'; 
-                nav.style.top=0+'px';
+               search.style.top= 0+'px';
+                nav.style.top=80+'px';
             }  
         }  
     }  
